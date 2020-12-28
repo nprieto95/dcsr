3536e9757893890e92a4bb8ed1c27b7755576c8d
Nahuel Agustín Prieto Galardo <nprietogalardo@uade.edu.ar>
2020-12-28 13:17:23 -0300

    created receiver project, transmitter now working correctly on earth

---
+++ Dcsr.ReplicationSender.Receiver/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from %s!\n", "Dcsr_ReplicationSender_Receiver");
+    return 0;
+}
--- Dcsr.ReplicationSender.Transmitter/main.cpp
+++ Dcsr.ReplicationSender.Transmitter/main.cpp
@@ -1,11 +1,23 @@
 #include <cstdio>
 #include <ams.h>
+#include <exception>
+#include <string>
+#include <limits.h>
+#include <unistd.h>
+
+char* getexepath()
+{
+	char result[PATH_MAX];
+	ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+	return result;
+}
 
 static AmsModule me;
 
 int main()
 {
-    ams_register("~/Dcsr/ION/Earth/mib.amsrc", NULL, NULL, NULL, 0, "amsdemo", "test", "",
-        "pitch", &me);
+	int regustrationResult = ams_register("/home/nahuel/Dcsr/ION/Earth/mib.amsrc", NULL, "dcsr", "uade", "", "transmitter", &me);
+	//printf("%d", regustrationResult);
+	//printf(getexepath());
     return 0;
 }
