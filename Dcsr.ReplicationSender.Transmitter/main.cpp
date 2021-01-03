4aef2735389e2d8ddf8c1f43ca24fe3df4b4dee5
Nahuel Agustín Prieto Galardo <nprietogalardo@uade.edu.ar>
2021-01-03 12:39:04 -0300

    wip

--- Dcsr.ReplicationSender.Transmitter/main.cpp
+++ Dcsr.ReplicationSender.Transmitter/main.cpp
@@ -1,66 +1,7 @@
-#include <ams.h>
-
-constexpr auto MARS_CONTINUUM = 2;
-constexpr auto DELTA_SUBJECT = 2;
+#include <iostream>
 
 int main()
 {
-	AmsModule module;
-	char mibSource[] = "/home/nahuel/Dcsr/ION/Earth/mib.amsrc";
-	char applicationName[] = "dcsr";
-	char authorityName[] = "uade";
-	char unitName[] = "";
-	char roleName[] = "transmitter";
-	int registrationFailed = ams_register(mibSource, NULL, applicationName, authorityName, unitName, roleName, &module);
-	if (!registrationFailed)
-		writeMemo("Transmitter successfully registered.");
-	else
-		return -1;
-
-	AmsEvent currentEvent;
-
-	int continuumNbr, unitNbr, moduleNbr, subjectNbr, contentLength, context, priority, roleNbr, domainContinuumNbr, domainUnitNbr;
-	unsigned char flowLabel;
-	char* content;
-	AmsMsgType msgType;
-	AmsStateType stateType;
-	AmsChangeType changeType;
-	AmsSequence sequence;
-	AmsDiligence diligence;
-
-
-	char message[] = "hello from the pale blue dot";
-
-	/*while (!ams_get_event(module, -1, &currentEvent))
-	{
-		switch (ams_get_event_type(currentEvent))
-		{
-		case NOTICE_EVT:
-			ams_parse_notice(currentEvent, &stateType, &changeType, &unitNbr, &moduleNbr, &roleNbr, &domainContinuumNbr, &domainUnitNbr, &subjectNbr, &priority, &flowLabel, &sequence, &diligence);
-			if (stateType == AmsInvitationState && changeType == AmsStateBegins)
-				ams_send(module, MARS_CONTINUUM, 0, moduleNbr, subjectNbr, priority, flowLabel, 28, message, 0);
-			break;
-		default:
-			break;
-		}
-	}*/
-
-	while (true)
-	{
-		int publishingFailed = ams_publish(module, DELTA_SUBJECT, 1, 0, 28, message, 0);
-		if (publishingFailed)
-			break;
-		//sleep(30);
-		/*int sendingFailed = ams_send(module, MARS_CONTINUUM, 0, 1, DELTA_SUBJECT, 1, 0, 28, message, 0);
-		if (sendingFailed)
-			break;
-		sleep(30);*/
-	}
-
-	int unregistrationFailed = ams_unregister(module);
-	if (!unregistrationFailed)
-		writeMemo("Transmitter successfully unregistered.");
-	else
-		return -1;
-	return 0;
+    std::cout << "Hey";
+    return 0;
 }
--- node_setup.sh
+++ node_setup.sh
@@ -2,4 +2,5 @@ sudo apt install make
 sudo ln -s /usr/bin/make /usr/bin/gmake
 sudo apt install gcc
 sudo apt-get install openssh-server g++ gdb make ninja-build rsync zip
+sudo systemctl enable ssh
 sudo service ssh start
