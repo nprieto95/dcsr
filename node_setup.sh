b98ff54626427bcc895f76b960a131f745501f48
Nahuel Prieto <nprietogalardo@uade.edu.ar>
2021-01-23 11:28:00 -0300

    earth working with CFDP

--- node_setup.sh
+++ node_setup.sh
@@ -4,3 +4,7 @@ sudo apt install gcc
 sudo apt-get install openssh-server g++ gdb make ninja-build rsync zip
 sudo systemctl enable ssh
 sudo service ssh start
+sudo apt-get install libcpprest-dev
+sudo apt-get install libxml2-dev uuid-dev
+sudo apt install cmake
+sudo apt-get install g++ git libboost-atomic-dev libboost-thread-dev libboost-system-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-random-dev libboost-chrono-dev libboost-serialization-dev libwebsocketpp-dev openssl libssl-dev ninja-build
